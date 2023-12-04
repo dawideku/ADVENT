251272933af1b1ab35f8d90c9d8442c7b67ea1a8
Dawid <260306@student.pwr.edu.pl>
2023-12-04 13:50:44 +0100

    4 day - 1st and 2nd part

--- Day4/main.py
+++ Day4/main.py
@@ -1,11 +1,13 @@
 curr_amount_of_cards = []
-sum = 0
+sum_1part = 0
+sum_2part = 0
 for i in range(0,198):
     curr_amount_of_cards.append(1)
 f = open('dane','r')
 counter = 0
 for line in f.readlines():
     matching = 0
+    result = 0
     line = line[line.index(':') + 2:]
     line = line.rstrip("\n")
     line = line.split(' | ')
@@ -14,10 +16,16 @@ for line in f.readlines():
     for el in yours:
         if el in winning:
             matching += 1
+            result += 1
+    if result == 1:
+        sum_1part += 1
+    elif result > 1:
+        sum_1part += 2**(result-1)
     for j in range(0, matching):
         curr_amount_of_cards[counter+1+j] += curr_amount_of_cards[counter]
     counter += 1
 
 for element in curr_amount_of_cards:
-    sum+=element
-print(sum)
+    sum_2part += element
+print(sum_1part)
+print(sum_2part)
